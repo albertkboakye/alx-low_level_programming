488218fba2ce6856f79ee5b2356e14ddddc49e0d
Albert Boakye <noblejack27@gmail.com>
2021-08-31 11:42:33 -0700

    program that prints Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *
+ *Description: prints holberton on new line
+ *
+ *Return: return 0 (success)
+ *
+ */
+int main(void)
+{
+	char c[10] = "Holberton";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(c[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
