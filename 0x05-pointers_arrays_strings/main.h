552987d604acd59b54f5214a9a468aec1572fd26
Albert Boakye <noblejack27@gmail.com>
2021-09-06 09:25:43 -0700

    prototype file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef main_h
+#define main_h
+
+/**
+* void prototypes()
+* int prototypes()
+*/
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+
+#endif /*main_h*/
